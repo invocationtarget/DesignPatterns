5de4179a2327a6e722eff12426ea5d1a605f7617
Marek <Marek@192.168.1.109>
2014-12-07 01:52:30 +0100

    init

---
+++ mavenproject1/src/main/java/earth/invocationtarget/mavenproject1/DP1Test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package earth.invocationtarget.mavenproject1;
+
+/**
+ *
+ * @author Marek
+ */
+public class DP1Test {
+
+        /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+}
